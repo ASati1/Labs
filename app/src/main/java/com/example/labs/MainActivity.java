11100e41934fb79cd4f0c07cf4076c4d1e3b7c9e
ASati1 <A.SATI-2020@hull.ac.uk>
2022-10-11 10:33:16 +0100

    Done part 1 and half of part 2 of lab 2

--- app/src/main/java/com/example/labs/MainActivity.java
+++ app/src/main/java/com/example/labs/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.labs;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import java.util.Random;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(  "Activity Lifecycle", "onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(  "Activity Lifecycle", "onResume");
+        TextView coinTossView = (TextView) findViewById(R.id.coinTossView);
+
+        String result = getCoinToss();
+
+        coinTossView.setText(result);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i( "Activity Lifecycle", "onCreate");
+    }
+    private String getCoinToss(){
+        if (random.nextBoolean()){
+            return "Heads";
+        }
+        return "Tails";
     }
 }
