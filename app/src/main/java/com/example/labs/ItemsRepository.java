7344d89e1cfb81cf0c609547a3938639109b17d7
ASati1 <A.SATI-2020@hull.ac.uk>
2022-11-01 09:47:34 +0000

    Defined a json object request to add to the queue via a GET method.

--- app/src/main/java/com/example/labs/ItemsRepository.java
+++ app/src/main/java/com/example/labs/ItemsRepository.java
@@ -1,6 +1,13 @@
 package com.example.labs;
 
+import android.content.ClipData;
 import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ItemsRepository {
     public static ItemsRepository sItemsRepository;
@@ -10,11 +17,36 @@ public class ItemsRepository {
     private ItemsRepository(Context pApplicationContext){
         this.mApplicationContext = pApplicationContext;
     }
-
+    
     public static ItemsRepository getInstance(Context pApplicationContext){
         if(sItemsRepository ==null){
             sItemsRepository = new ItemsRepository(pApplicationContext);
         }
         return sItemsRepository;
+    }
+    
+    public LiveData<ArrayList<ClipData.Item>> loadItemsFromJSON(){
+        RequestQueue queue = Volley.newRequestQueue(mApplicationContext);
+        String url = "https://www.goparker.com/600096/index.json";
+        
+        //Request a jsonObject response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.Get,
+                url,
+                null,
+                new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject response){
+                        
+                        
+                    }
+        },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        String errorResponse = "That didn't work!";
+                    }
+                }
+        );
     }
 }
